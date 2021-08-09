29f7e64ab4209e7cb67d259104b8510170db4599
Lucas Lima <lucaslimapoa3@gmail.com>
2021-08-09 22:22:50 +0200

    feat: adds closure initialiser to SelectState

--- Sources/SwiftRedux/View Hooks/SelectState.swift
+++ Sources/SwiftRedux/View Hooks/SelectState.swift
@@ -11,13 +11,19 @@ import SwiftUI
 @propertyWrapper
 public struct SelectState<RootState, Value>: DynamicProperty where RootState: Equatable {
     @EnvironmentObject private var store: Store<RootState>
-    private let keyPath: KeyPath<RootState, Value>
+    private let transform: (RootState) -> Value
     
     public var wrappedValue: Value {
-        store.state[keyPath: keyPath]
+        transform(store.state)
     }
     
-    public init(_ keyPath: KeyPath<RootState, Value>) where Value: Equatable, RootState: Equatable {
-        self.keyPath = keyPath
+    public init(_ keyPath: KeyPath<RootState, Value>) {
+        self.transform = { state in
+            state[keyPath: keyPath]
+        }
+    }
+    
+    public init(_ transform: @escaping (RootState) -> Value) {
+        self.transform = transform
     }
 }
