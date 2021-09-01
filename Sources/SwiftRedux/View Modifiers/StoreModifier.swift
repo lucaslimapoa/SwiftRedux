5648c2743940752e6a47c68a9507d1ad9951a9cf
Lucas Lima <lucaslimapoa3@gmail.com>
2021-09-01 21:12:50 +0200

    docs: adds documentation for StoreModifier

--- Sources/SwiftRedux/View Modifiers/StoreModifier.swift
+++ Sources/SwiftRedux/View Modifiers/StoreModifier.swift
@@ -8,6 +8,17 @@
 import SwiftUI
 
 extension View {
+    /**
+     Adds an instance of the a Store to the SwiftUI Environment, so that the `@SelectState` and `@Dispatch` property wrappers can be used.
+     The store should be inserted in the first view where the Store's state is used, so that children view will also have access to it.
+     
+     - Parameter store: A store instance to be set in the SwiftUI Environment.
+       
+     ```
+     InitialView()
+         .store(SomeStore())
+     ```
+    */
     @discardableResult
     public func store<S>(_ store: S) -> some View where S: Storable, S: ObservableObject, S.State: Equatable {
         // Need to find a way to unit test this
