e4c92c527676ac14624838da25f9e06483b72428
Lucas Lima <lucaslimapoa3@gmail.com>
2021-09-01 21:02:44 +0200

    docs: adds documentation for Dispatch property wrapper

--- Sources/SwiftRedux/View Hooks/Dispatch.swift
+++ Sources/SwiftRedux/View Hooks/Dispatch.swift
@@ -7,6 +7,19 @@
 
 import SwiftUI
 
+/**
+ A property wrapper for extracting the Dispatch function of the Store in the SwiftUI view environment.
+ ```
+ struct SomeView: View {
+    @Dispatch<AnyAction> private var dispatch
+    
+    var body: some View {
+        Text("Hello")
+            .onAppear { dispatch(action: FetchAction.loadAll) }
+    }
+ }
+ ```
+*/
 @propertyWrapper
 public struct Dispatch<ActionType>: DynamicProperty {
     @Environment(\.dispatch) private var dispatch
