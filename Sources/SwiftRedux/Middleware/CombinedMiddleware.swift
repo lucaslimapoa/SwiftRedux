831ac0090ea691e3c251eb9affd78b74535731cf
Lucas Lima <lucaslimapoa3@gmail.com>
2021-09-01 21:34:10 +0200

    docs: adds documentation for CombinedMiddleware

--- Sources/SwiftRedux/Middleware/CombinedMiddleware.swift
+++ Sources/SwiftRedux/Middleware/CombinedMiddleware.swift
@@ -7,13 +7,38 @@
 
 import Foundation
 
+/**
+ A convenience object for combining multiple middlware into one.
+ */
 public struct CombinedMiddleware<State>: Middleware {
     private let runClosure: (_ store: StoreProxy<State>, _ next: (AnyAction) -> Void, _ action: AnyAction) -> Void
     
+    /**
+     Combines multiple middleware into a single middleware. It's important to notice that the order the middleware applied is the order they will run.
+     
+     - Parameter middleware: A middleware to combine.
+     
+     ```
+     CombinedMiddleware<AppState>
+        .apply(LoggingMiddleware())
+        .apply(ThunkMiddleware())
+     ```
+     */
     public static func apply<M>(_ middleware: M) -> CombinedMiddleware<State> where M: Middleware, M.State == State {
         CombinedMiddleware(runClosure: middleware.run(store:next:action:))
     }
 
+    /**
+     Combines multiple middleware into a single middleware. It's important to notice that the order the middleware applied is the order they will run.
+     
+     - Parameter anotherMiddleware: A middleware to combine.
+     
+     ```
+     CombinedMiddleware<AppState>
+        .apply(LoggingMiddleware())
+        .apply(ThunkMiddleware())
+     ```
+     */
     public func apply<M>(_ anotherMiddleware: M) -> CombinedMiddleware<State> where M: Middleware, M.State == State {
         CombinedMiddleware { store, next, action in
             let nextMiddleware: (AnyAction) -> Void = { anotherAction in
@@ -23,6 +48,9 @@ public struct CombinedMiddleware<State>: Middleware {
         }
     }
 
+    /**
+     A function that will run all combined middleware in the order they were applied.
+     */
     public func run(store: StoreProxy<State>, next: (AnyAction) -> Void, action: AnyAction) {
         runClosure(store, next, action)
     }
